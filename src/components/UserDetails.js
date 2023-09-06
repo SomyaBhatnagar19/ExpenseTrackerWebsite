da00ed31de45f40fa47f7bddbcee9a28e436f7be
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-09-06 10:44:11 +0530

    Making the userDeatil compoentn css fetch and display the table format data.

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -13,15 +13,18 @@ const UserDetails = () => {
       return;
     }
 
-    fetch("https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCA54c2FvusfrWM1tu6REcI_H-OVsXTm84", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        idToken: idToken,
-      }),
-    })
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCA54c2FvusfrWM1tu6REcI_H-OVsXTm84",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          idToken: idToken,
+        }),
+      }
+    )
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
@@ -49,36 +52,56 @@ const UserDetails = () => {
 
   return (
     <div>
-        <div className="bg-stone-200 p-5 text-right">
-              <span className="bg-emerald-500 text-white text-sm p-1 rounded-xl">
-                Your profile is now 100% complete.
-              </span>          
-        </div>
-        <h2 className="text-2xl font-semibold m-4 text-center">User Details</h2>
-      <div className="container mx-auto mt-5">
+      <div className="bg-stone-200 p-5 flex justify-between shadow-lg">
+        <h4 className="text-lg italic font-semibold border-l-4 border-emerald-700 pl-4 text-emerald-700">
+          Hello {userData.fullName}!! Welcome to Expense Tracker
+        </h4>
+        <span className="bg-emerald-500 text-white text-right text-sm p-1 rounded-xl">
+          Your profile is now 100% complete.
+        </span>
+      </div>
+
+      <div className="container mx-auto mt-8">
         {isLoading ? (
           <div className="flex items-center justify-center">
-          <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-emerald-500 "></div>
-        </div>
+            <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-emerald-500 "></div>
+          </div>
         ) : (
-            <div className="bg-emerald-50 shadow-md rounded-lg p-4">
-            <div className="grid grid-cols-2 gap-4">
-              <div className="col-span-2 sm:col-span-1">
-                <p className="font-semibold text-lg mb-2">Full Name:</p>
-                <p className="text-gray-800">{userData.fullName}</p>
-              </div>
-              <div className="col-span-2 sm:col-span-1">
-                <p className="font-semibold text-lg mb-2">Email:</p>
-                <p className="text-gray-800">{userData.email}</p>
-              </div>
-              <div className="col-span-2">
-                <p className="font-semibold text-lg mb-2">Profile Photo URL:</p>
-                <p className="text-gray-800 break-all">{userData.profilePhotoUrl}</p>
-              </div>
+          <div className="shadow-md rounded-lg">
+            <div className="border-l-4 border-emerald-700 bg-stone-100 shadow-md">
+              <h2 className="text-xl italic ml-1 mb-1 font-semibold">
+                User Details
+              </h2>
+            </div>
+            <div className="p-3 shadow-md">
+            <table className="w-full border border-gray-300 rounded-md">
+              <tbody>
+                <tr className="border-b border-gray-300">
+                  <td className="px-4 py-3 font-semibold text-md">
+                    Full Name:
+                  </td>
+                  <td className="px-4 py-3 text-md text-gray-800">
+                    {userData.fullName}
+                  </td>
+                </tr>
+                <tr className="border-b border-gray-300">
+                  <td className="px-4 py-3 font-semibold text-md">Email:</td>
+                  <td className="px-4 py-3 text-md text-gray-800">
+                    {userData.email}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-4 py-3 font-semibold text-md">
+                    Profile Photo URL:
+                  </td>
+                  <td className="px-4 py-3 text-md text-gray-800 break-all">
+                    {userData.profilePhotoUrl}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
             </div>
           </div>
-          
-
         )}
       </div>
     </div>
