326311516d56312b59c641b18e9a8643af144c2d
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-09-12 12:57:13 +0530

    Logout functionality still not added to header. The expenses still not user specific but the edit and deelete functionality are working properly.

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-//icon
 import dropdownIcon from "./assets/dropdown.png";
 import AddExpenseIcon from "./assets/addExpense.png";
 import ExpenseReportIcon from "./assets/expenseReport.png";
-
-//file
 import ExpenseReport from "./ExpenseReport";
 import Header from "./Header";
 
-const ExpenseForm = ({ onExpenseSubmit }) => {
+const ExpenseForm = () => {
   const [expenseData, setExpenseData] = useState({
     amount: "",
     description: "",
@@ -17,19 +14,22 @@ const ExpenseForm = ({ onExpenseSubmit }) => {
   });
 
   const [expenses, setExpenses] = useState([]);
+  const [editingExpense, setEditingExpense] = useState(null);
+  const [message, setMessage] = useState("");
+  // Include a user identifier, e.g., userId, in the API URL
+  const userId = `${expenseData.id}`; // Replace with the actual user ID
+  const realtimeDbFirebase = `https://expensetracker-20504-default-rtdb.firebaseio.com/Users/${userId}/Expenses.json`;
 
-
-  const realtimeDbFirebase =
-    "https://expensetracker-20504-default-rtdb.firebaseio.com/Expenses.json";
-
-  // Function to fetch expenses as soon as the user adds making the useeffect for same below
   const fetchExpenses = () => {
     axios
       .get(realtimeDbFirebase)
       .then((response) => {
         const data = response.data;
         if (data) {
-          const expensesArray = Object.values(data);
+          const expensesArray = Object.keys(data).map((key) => ({
+            id: key, // Add a unique id for each expense
+            ...data[key],
+          }));
           setExpenses(expensesArray);
         }
       })
@@ -39,7 +39,7 @@ const ExpenseForm = ({ onExpenseSubmit }) => {
   };
 
   useEffect(() => {
-    fetchExpenses(); // Fetch expenses when the component mounts
+    fetchExpenses();
   }, []);
 
   const handleChange = (e) => {
@@ -53,140 +53,199 @@ const ExpenseForm = ({ onExpenseSubmit }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    axios
-      .post(realtimeDbFirebase, expenseData)
-      .then((response) => {
-        console.log("Expense added successfully:", response);
-        setExpenseData({
-          amount: "",
-          description: "",
-          category: "",
+    if (editingExpense) {
+      // Editing an existing expense
+      axios
+        .put(
+          `https://expensetracker-20504-default-rtdb.firebaseio.com/Users/${userId}/Expenses/${editingExpense.id}.json`,
+          expenseData
+        )
+        .then((response) => {
+          console.log("Expense edited successfully:", response);
+          fetchExpenses();
+          setEditingExpense(null);
+        })
+        .catch((error) => {
+          console.error("Error editing expense:", error);
         });
+    } else {
+      // Adding a new expense
+      axios
+        .post(realtimeDbFirebase, expenseData)
+        .then((response) => {
+          console.log("Expense added successfully:", response);
+          setExpenseData({
+            amount: "",
+            description: "",
+            category: "",
+          });
 
-        // Fetch updated expenses after adding a new one
+          fetchExpenses();
+        })
+        .catch((error) => {
+          console.error("Error adding expense:", error);
+        });
+    }
+  };
+
+  const handleDelete = (itemId) => {
+    axios
+      .delete(
+        `https://expensetracker-20504-default-rtdb.firebaseio.com/Users/${userId}/Expenses/${itemId}.json`
+      )
+      .then((response) => {
+        console.log("Expense deleted successfully:", response);
+        setMessage("Expense deleted successfully:", response);
         fetchExpenses();
       })
       .catch((error) => {
-        console.error("Error adding expense:", error);
+        console.error("Error deleting expense:", error);
+        setMessage("Error deleting expense:", error);
       });
   };
+
+  const handleEdit = (editedItem) => {
+    setEditingExpense(editedItem);
+    setExpenseData({
+      amount: editedItem.amount,
+      description: editedItem.description,
+      category: editedItem.category,
+    });
+    setMessage("Expense Edited Successfully.")
+  };
+
   return (
     <>
-    <Header/>
-    
-    <div className="bg-stone-100 min-h-screen p-10">
-      <div className="bg-white shadow-md w-full sm:w-full xl:w-full">
-        <div className="bg-stone-200 sm:flex sm:justify-between shadow-lg">
-          <h4 className="text-lg italic font-semibold border-l-4 border-emerald-700 pl-4 text-emerald-700 flex items-center justify-between">
-            Add New Expense
-            
+      <Header />
+      <div className="bg-stone-100 min-h-screen p-10">
+        <div className="bg-white shadow-md w-full sm:w-full xl:w-full">
+          <div className="bg-stone-200 sm:flex sm:justify-between shadow-lg">
+            <h4 className="text-lg italic font-semibold border-l-4 border-emerald-700 pl-4 text-emerald-700 flex items-center justify-between">
+              Add New Expense
               <img
                 src={AddExpenseIcon}
                 alt="addExpense-icon"
                 className="w-10 h-10 p-1"
               />
-            
-          </h4>
-        </div>
-
-        <div className="p-3 shadow-md mt-4">
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            </h4>
+          </div>
+          
+          <div className="p-3 shadow-md mt-4">
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                <div>
+                  <label className="text-gray-700 text-md font-semibold mb-2">
+                    Amount
+                  </label>
+                  <input
+                    type="number"
+                    name="amount"
+                    value={expenseData.amount}
+                    onChange={handleChange}
+                    className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
+                    placeholder="Enter amount"
+                    required
+                  />
+                </div>
+                <div className="relative">
+                  <label className="text-gray-700 text-md font-semibold mb-2">
+                    Category
+                  </label>
+                  <select
+                    name="category"
+                    value={expenseData.category}
+                    onChange={handleChange}
+                    className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
+                    required
+                  >
+                    <option value="" disabled>
+                      Select category of expense
+                    </option>
+                    <option value="Salary">Salary</option>
+                    <option value="Fruits">Fruits</option>
+                    <option value="Vegetable">Vegetable</option>
+                    <option value="Necessity/Body Care">
+                      Necessity/Body Care
+                    </option>
+                    <option value="Snacks/ Junk Food">Snacks/ Junk Food</option>
+                    <option value="Fuel/Travel">Fuel/Travel</option>
+                    <option value="Education">Education</option>
+                    <option value="Electronics">Electronics</option>
+                  </select>
+                  <div className="absolute inset-y-0 right-0 flex items-center pt-6 pr-2 pointer-events-none">
+                    <img
+                      src={dropdownIcon}
+                      alt="dropdown-icon"
+                      className="w-5 h-5 bg-emerald-500 rounded-2xl"
+                    />
+                  </div>
+                </div>
+              </div>
               <div>
                 <label className="text-gray-700 text-md font-semibold mb-2">
-                  Amount
+                  Description
                 </label>
                 <input
-                  type="number"
-                  name="amount"
-                  value={expenseData.amount}
+                  type="text"
+                  name="description"
+                  value={expenseData.description}
                   onChange={handleChange}
                   className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
-                  placeholder="Enter amount"
+                  placeholder="Enter description"
                   required
                 />
               </div>
-              <div className="relative">
-                <label className="text-gray-700 text-md font-semibold mb-2">
-                  Category
-                </label>
-                <select
-                  name="category"
-                  value={expenseData.category}
-                  onChange={handleChange}
-                  className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
-                  required
-                >
-                  <option value="" disabled>
-                    Select category of expense
-                  </option>
-                  <option value="Salary">Salary</option>
-                  <option value="Fruits">Fruits</option>
-                  <option value="Vegetable">Vegetable</option>
-                  <option value="Necessity/Body Care">
-                    Necessity/Body Care
-                  </option>
-                  <option value="Snacks/ Junk Food">Snacks/ Junk Food</option>
-                  <option value="Fuel/Travel">Fuel/Travel</option>
-                  <option value="Education">Education</option>
-                  <option value="Electronics">Electronics</option>
-                </select>
-                <div className="absolute inset-y-0 right-0 flex items-center pt-6 pr-2 pointer-events-none">
-                  <img
-                    src={dropdownIcon}
-                    alt="dropdown-icon"
-                    className="w-5 h-5 bg-emerald-500 rounded-2xl"
-                  />
-                </div>
-              </div>
-            </div>
-            <div>
-              <label className="text-gray-700 text-md font-semibold mb-2">
-                Description
-              </label>
-              <input
-                type="text"
-                name="description"
-                value={expenseData.description}
-                onChange={handleChange}
-                className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
-                placeholder="Enter description"
-                required
-              />
-            </div>
-            <hr className="border border-emerald-500 my-2"></hr>
-            <button
-              type="submit"
-              className="bg-emerald-700 text-white p-2 rounded hover:bg-emerald-600 "
-            >
-              Save Expense
-            </button>
-          </form>
-        </div>
-        <div className="bg-white shadow-md">
-          <div className="bg-stone-200 sm:flex sm:justify-between shadow-lg">
-            <h4 className="text-lg italic font-semibold border-l-4 border-emerald-700 pl-4 text-emerald-700 flex items-center justify-between">
-              Expense Report
+              <hr className="border border-emerald-500 my-2"></hr>
+              <button
+                type="submit"
+                className="bg-emerald-700 text-white p-2 rounded hover:bg-emerald-600"
+              >
+                
+                {editingExpense ? "Save Expense" : "Add Expense"}
+              </button>
               
+              {editingExpense && (
+                <button
+                  type="button"
+                  className="bg-red-400 text-white p-2 rounded hover:bg-gray-300 ml-2"
+                  onClick={() => setEditingExpense(null)}
+                >
+                  Cancel Edit
+                </button>
+              )}
+              {message && (
+            <div className="text-green-500 text-center italic my-4">
+              {message}
+            </div>
+          )}
+            </form>
+          </div>
+          <div className="bg-white shadow-md">
+            <div className="bg-stone-200 sm:flex sm:justify-between shadow-lg">
+              <h4 className="text-lg italic font-semibold border-l-4 border-emerald-700 pl-4 text-emerald-700 flex items-center justify-between">
+                Expense Report
                 <img
                   src={ExpenseReportIcon}
                   alt="expenseReport-icon"
                   className="w-10 h-10 p-1"
                 />
-              
-            </h4>
+              </h4>
+            </div>
+            {expenses.length > 0 ? (
+              <ExpenseReport
+                expenses={expenses}
+                onDelete={handleDelete}
+                onEdit={handleEdit}
+              />
+            ) : (
+              <p className="p-1">No expenses to display.</p>
+            )}
           </div>
-
-          {expenses.length > 0 ? (
-            <ExpenseReport expenses={expenses} />
-          ) : (
-            <p>No expenses to display.</p>
-          )}
         </div>
       </div>
-    </div>
     </>
   );
 };
 
 export default ExpenseForm;
+
--- src/components/ExpenseReport.js
+++ src/components/ExpenseReport.js
@@ -1,8 +1,43 @@
+// import React from "react";
+// import EditBtn from './assets/Edit.png';
+// import DeleteBtn from './assets/Delete.png';
+
+// const ExpenseReport = ({ expenses, onDelete, onEdit }) => {
+//   return (
+//     <div className="bg-white shadow-md p-6 overflow-x-auto">
+//       <table className="w-full border-collapse border border-gray-300 shadow-md">
+//         <thead>
+//           <tr>
+//             <th className="p-2 border border-gray-300">Description</th>
+//             <th className="p-2 border border-gray-300">Category</th>
+//             <th className="p-2 border border-gray-300">Amount</th>
+//             <th className="p-2 border border-gray-300 w-20">Actions</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {expenses.map((expense, index) => (
+//             <tr key={index}>
+//               <td className="p-2 border border-gray-300">{expense.description}</td>
+//               <td className="p-2 border border-gray-300">{expense.category}</td>
+//               <td className="p-2 border border-gray-300">Rs. {expense.amount}</td>
+//               <td className="p-2 border border-gray-300 w-20 flex justify-center items-center">
+//                 <button className="text-sm border-r pr-1" onClick={() => onEdit(expense)}><img src={EditBtn} alt="edit-btn" /></button>
+//                 <button className="text-sm ml-1" onClick={() => onDelete(expense.id)}><img src={DeleteBtn} alt="delete-btn" /></button>
+//               </td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
+// export default ExpenseReport;
 import React from "react";
-//icons
 import EditBtn from './assets/Edit.png';
 import DeleteBtn from './assets/Delete.png';
-const ExpenseReport = ({ expenses }) => {
+
+const ExpenseReport = ({ expenses, onDelete, onEdit }) => {
   return (
     <div className="bg-white shadow-md p-6 overflow-x-auto">
       <table className="w-full border-collapse border border-gray-300 shadow-md">
@@ -21,8 +56,8 @@ const ExpenseReport = ({ expenses }) => {
               <td className="p-2 border border-gray-300">{expense.category}</td>
               <td className="p-2 border border-gray-300">Rs. {expense.amount}</td>
               <td className="p-2 border border-gray-300 w-20 flex justify-center items-center">
-                <button className="text-sm border-r pr-1"><img src={EditBtn} alt="edit-btn"/></button>
-                <button className="text-sm ml-1"><img src={DeleteBtn} alt="edit-btn"/></button>
+                <button className="text-sm border-r pr-1" onClick={() => onEdit(expense)}><img src={EditBtn} alt="edit-btn" /></button>
+                <button className="text-sm ml-1" onClick={() => onDelete(expense.id)}><img src={DeleteBtn} alt="delete-btn" /></button>
               </td>
             </tr>
           ))}
