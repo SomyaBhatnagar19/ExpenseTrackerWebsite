35f9e6ce4d0bcae4cd4dc82e2cdc636b665fe1b5
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-09-21 13:09:35 +0530

    Making the download csv option for the report.

--- src/components/ExpenseReport.js
+++ src/components/ExpenseReport.js
@@ -1,10 +1,30 @@
 import React from "react";
-import EditBtn from './assets/Edit.png';
-import DeleteBtn from './assets/Delete.png';
+import EditBtn from "./assets/Edit.png";
+import DeleteBtn from "./assets/Delete.png";
 
 const ExpenseReport = ({ expenses, onDelete, onEdit }) => {
   // Calculate total expense
-  const totalExpense = expenses.reduce((total, expense) => total + parseFloat(expense.amount), 0);
+  const totalExpense = expenses.reduce(
+    (total, expense) => total + parseFloat(expense.amount),
+    0
+  );
+
+  const generateCsvContent = () => {
+    const header = "Description,Category,Amount\n";
+    const csvData = expenses
+      .map((expense) => `${expense.description},${expense.category},${expense.amount}`)
+      .join("\n");
+    return header + csvData;
+  };
+
+  const downloadCsv = () => {
+    const csvContent = generateCsvContent();
+    const blob = new Blob([csvContent], { type: "text/csv" });
+    const link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "expenses.csv";
+    link.click();
+  };
 
   return (
     <div className="bg-white shadow-md p-6 overflow-x-auto">
@@ -24,8 +44,12 @@ const ExpenseReport = ({ expenses, onDelete, onEdit }) => {
               <td className="p-2 border border-gray-300">{expense.category}</td>
               <td className="p-2 border border-gray-300">Rs. {expense.amount}</td>
               <td className="p-2 border border-gray-300 w-20 flex justify-center items-center">
-                <button className="text-sm border-r pr-1" onClick={() => onEdit(expense)}><img src={EditBtn} alt="edit-btn" /></button>
-                <button className="text-sm ml-1" onClick={() => onDelete(expense.id)}><img src={DeleteBtn} alt="delete-btn" /></button>
+                <button className="text-sm border-r pr-1" onClick={() => onEdit(expense)}>
+                  <img src={EditBtn} alt="edit-btn" />
+                </button>
+                <button className="text-sm ml-1" onClick={() => onDelete(expense.id)}>
+                  <img src={DeleteBtn} alt="delete-btn" />
+                </button>
               </td>
             </tr>
           ))}
@@ -33,8 +57,12 @@ const ExpenseReport = ({ expenses, onDelete, onEdit }) => {
       </table>
 
       <div className="flex justify-between items-center p-2">
-        <span className="text-left text-lg font-semibold text-emerald-700 italic">Total Expense</span>
-        <span className="text-right text-md font-bold text-emerald-700 italic">Rs. {totalExpense} /-</span>
+        <span className="text-left text-lg font-semibold text-emerald-700 italic">
+          Total Expense
+        </span>
+        <span className="text-right text-md font-bold text-emerald-700 italic">
+          Rs. {totalExpense} /-
+        </span>
         {totalExpense > 10000 && (
           <button
             type="button"
